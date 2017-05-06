52499a069d663db1cdd422980292b77e97ed1ad5
Zaikin Alexander <Raggel@mail.ru>
2017-05-06 20:12:18 +0500

    -начальная стадия проекта

---
+++ DataImages.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MNIST_basic
+{
+
+    public class Pattern
+    {
+        public byte val;
+        public byte[] rawData;
+        public double[] data;
+
+        public Pattern()
+        {
+            rawData = new byte[784];
+            data = new double[784];
+        }
+
+        public void normalize()
+        {
+            for (int i = 0; i < 784; i++)
+                data[i] = 2.0 / 255 * rawData[i] - 1;
+        }
+    }
+
+
+    public class DataImages
+    {
+        string fileName;
+        MemoryStream memStream;
+
+        public void loadPattern(int index, Pattern p)
+        {
+            memStream.Position = index * (p.data.Length + 1);
+            p.val = (Byte)memStream.ReadByte();
+            memStream.Read(p.rawData, 0, p.data.Length);
+            p.normalize();
+        }
+
+        public DataImages(string fileName)
+        {
+            this.fileName = fileName;
+            memStream = new MemoryStream();
+            FileStream fs = File.OpenRead(fileName);
+            fs.CopyTo(memStream);
+            fs.Close();
+        }
+
+        ~DataImages()
+        {
+            memStream.Close();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Basic_Neuron_Network;
+
+namespace MNIST_basic
+{
+
+    public partial class Form1 : Form
+    {
+        public DataImages TrainBaseMemStream = new DataImages(@"C:\Users\Саша\Documents\RAD Studio\Projects\NeuroNet_MNIS\Win32\Debug\TrainMNIST-60k.dat");
+        public DataImages TestBaseMemStream = new DataImages(@"C:\Users\Саша\Documents\RAD Studio\Projects\NeuroNet_MNIS\Win32\Debug\TestMNIST-10k.dat");
+        NeuronNetwork nNet;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nNet = new NeuronNetwork(784, 30);
+            nNet.addLayer(10, layerType.OutputLayer);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pattern p = new Pattern();
+            TrainBaseMemStream.loadPattern(2, p);
+            double[] target = new double[10];
+            for (int i = 0; i < 10; i++)
+                target[i] = 0;
+            target[p.val] = 1;
+            nNet.trainSingle(p.data, target);
+        }
+    }
+}
