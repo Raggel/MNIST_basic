54d605766be77e383e62671876fd27f4e0864b3d
Zaikin Alexander <Raggel@mail.ru>
2017-05-07 16:58:05 +0500

    -нерабочая тренировка

--- Form1.cs
+++ Form1.cs
@@ -39,5 +39,45 @@ namespace MNIST_basic
             target[p.val] = 1;
             nNet.trainSingle(p.data, target);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pattern p = new Pattern();
+            Random rnd = new Random();
+            int iRnd = new int();
+            double[] target = new double[10];
+            for (int i = 0; i < 100000; i++)
+            {
+                iRnd = rnd.Next(0, 60000 - 1);
+                TrainBaseMemStream.loadPattern(iRnd, p);
+                for (int j = 0; j < 10; j++)
+                    target[j] = 0;
+                target[p.val] = 1;
+                nNet.trainSingle(p.data, target);
+                this.Text = i.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Pattern p = new Pattern();
+            Random rnd = new Random();
+            int iRnd = new int();
+            iRnd = rnd.Next(0, 60000 - 1);
+            TrainBaseMemStream.loadPattern(iRnd, p);
+            nNet.getAnswer(p.data);
+            double[] result = nNet.getAnswer(p.data);
+            label1.Text = result[0].ToString("N4");
+            label2.Text = result[1].ToString("N4");
+            label3.Text = result[2].ToString("N4");
+            label4.Text = result[3].ToString("N4");
+            label5.Text = result[4].ToString("N4");
+            label6.Text = result[5].ToString("N4");
+            label7.Text = result[6].ToString("N4");
+            label8.Text = result[7].ToString("N4");
+            label9.Text = result[8].ToString("N4");
+            label10.Text = result[9].ToString("N4");
+            label11.Text = p.val.ToString();
+        }
     }
 }
