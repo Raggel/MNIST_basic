26d4fc62fc2bbe671bbfbbb250890a8df3689c78
Zaikin Alexander <Raggel@mail.ru>
2017-05-07 20:30:05 +0500

    -сохранение -speedbutton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,9 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
@@ -173,11 +176,46 @@
             this.label11.TabIndex = 14;
             this.label11.Text = "label11";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(93, 378);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Load";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(587, 267);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 23);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(924, 413);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -194,6 +232,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -216,6 +255,9 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,9 +14,10 @@ namespace MNIST_basic
 
     public partial class Form1 : Form
     {
-        public DataImages TrainBaseMemStream = new DataImages(@"C:\Users\Саша\Documents\RAD Studio\Projects\NeuroNet_MNIS\Win32\Debug\TrainMNIST-60k.dat");
-        public DataImages TestBaseMemStream = new DataImages(@"C:\Users\Саша\Documents\RAD Studio\Projects\NeuroNet_MNIS\Win32\Debug\TestMNIST-10k.dat");
+        public DataImages TrainBaseMemStream = new DataImages(Application.StartupPath + @"\TrainMNIST-60k.dat");
+        public DataImages TestBaseMemStream = new DataImages(Application.StartupPath + @"\TestMNIST-10k.dat");
         NeuronNetwork nNet;
+        public NeuronNetRepository NetRep = new NeuronNetRepository(Application.StartupPath + @"\Network1.dat");
 
         public Form1()
         {
@@ -25,8 +26,10 @@ namespace MNIST_basic
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            nNet = new NeuronNetwork(784, 30);
+            nNet = new NeuronNetwork(784, 300);
             nNet.addLayer(10, layerType.OutputLayer);
+            new NeuronNetRepository(Application.StartupPath + @"\Network1.dat");
+            NetRep.loadNet(nNet);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -46,9 +49,9 @@ namespace MNIST_basic
             Random rnd = new Random();
             int iRnd = new int();
             double[] target = new double[10];
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 1200000; i++)
             {
-                iRnd = rnd.Next(0, 60000 - 1);
+                iRnd = rnd.Next(0, 60000);
                 TrainBaseMemStream.loadPattern(iRnd, p);
                 for (int j = 0; j < 10; j++)
                     target[j] = 0;
@@ -63,21 +66,65 @@ namespace MNIST_basic
             Pattern p = new Pattern();
             Random rnd = new Random();
             int iRnd = new int();
-            iRnd = rnd.Next(0, 60000 - 1);
+            iRnd = rnd.Next(0, 60000);
             TrainBaseMemStream.loadPattern(iRnd, p);
             nNet.getAnswer(p.data);
             double[] result = nNet.getAnswer(p.data);
-            label1.Text = result[0].ToString("N4");
-            label2.Text = result[1].ToString("N4");
-            label3.Text = result[2].ToString("N4");
-            label4.Text = result[3].ToString("N4");
-            label5.Text = result[4].ToString("N4");
-            label6.Text = result[5].ToString("N4");
-            label7.Text = result[6].ToString("N4");
-            label8.Text = result[7].ToString("N4");
-            label9.Text = result[8].ToString("N4");
-            label10.Text = result[9].ToString("N4");
+            label1.Text = result[0].ToString("N8");
+            label2.Text = result[1].ToString("N8");
+            label3.Text = result[2].ToString("N8");
+            label4.Text = result[3].ToString("N8");
+            label5.Text = result[4].ToString("N8");
+            label6.Text = result[5].ToString("N8");
+            label7.Text = result[6].ToString("N8");
+            label8.Text = result[7].ToString("N8");
+            label9.Text = result[8].ToString("N8");
+            label10.Text = result[9].ToString("N8");
             label11.Text = p.val.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            NetRep.saveNet(nNet);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            NetRep.loadNet(nNet);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            NetRep.saveNet(nNet);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox1.Text = "Stop";
+            }
+            else
+            {
+                checkBox1.Text = "Start";
+            }
+            Pattern p = new Pattern();
+            Random rnd = new Random();
+            int iRnd;
+            int i = 0;
+            double[] target = new double[10];
+            while (checkBox1.Checked)
+            {
+                i++;
+                iRnd = rnd.Next(0, 60000);
+                TrainBaseMemStream.loadPattern(iRnd, p);
+                for (int j = 0; j < 10; j++)
+                    target[j] = 0;
+                target[p.val] = 1;
+                nNet.trainSingle(p.data, target);
+                this.Text = i.ToString();
+                Application.DoEvents();
+            }
         }
     }
 }
